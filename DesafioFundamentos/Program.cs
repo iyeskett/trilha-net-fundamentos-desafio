700c9a8a7bbc0c51c8bec8d7a00e936791ccc5c4
Felipe Mello <felipesilva.mello27@gmail.com>
2022-08-27 01:06:12 -0300

    O programa agora trata erros e verifica se o padrão da placa e os valores estão certos.

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,4 +1,7 @@
 ﻿using DesafioFundamentos.Models;
+using System.Globalization;
+
+
 
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -6,54 +9,86 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
-
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
-
-// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
-Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
-
-string opcao = string.Empty;
-bool exibirMenu = true;
-
-// Realiza o loop do menu
-while (exibirMenu)
+try
 {
-    Console.Clear();
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar veículo");
-    Console.WriteLine("2 - Remover veículo");
-    Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
-
-    switch (Console.ReadLine())
+    Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+                  "Digite o preço inicial:  Ex: 3,5");
+    do
     {
-        case "1":
-            es.AdicionarVeiculo();
+        precoInicial = Estacionamento.ConverterPreco(Console.ReadLine());
+        if (precoInicial >= 0)
+        {
             break;
+        }else
+            Console.WriteLine("Preço inválido, digite novamente.");
 
-        case "2":
-            es.RemoverVeiculo();
-            break;
+    } while (true);
 
-        case "3":
-            es.ListarVeiculos();
-            break;
 
-        case "4":
-            exibirMenu = false;
+        Console.WriteLine("Agora digite o preço por hora: Ex: 4,0");
+    do
+    {
+        precoPorHora = Estacionamento.ConverterPreco(Console.ReadLine());
+        if (precoPorHora >= 0)
+        {
             break;
+        }
+        else
+            Console.WriteLine("Preço inválido, digite novamente.");
 
-        default:
-            Console.WriteLine("Opção inválida");
-            break;
+    } while (true);
+
+    
+
+    // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+    Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+    string opcao = string.Empty;
+    bool exibirMenu = true;
+
+    // Realiza o loop do menu
+    while (exibirMenu)
+    {
+        Console.Clear();
+        Console.WriteLine("Digite a sua opção:");
+        Console.WriteLine("1 - Cadastrar veículo");
+        Console.WriteLine("2 - Remover veículo");
+        Console.WriteLine("3 - Listar veículos");
+        Console.WriteLine("4 - Encerrar");
+
+        switch (Console.ReadLine())
+        {
+            case "1":
+                es.AdicionarVeiculo();
+                break;
+
+            case "2":
+                es.RemoverVeiculo();
+                break;
+
+            case "3":
+                es.ListarVeiculos();
+                break;
+
+            case "4":
+                exibirMenu = false;
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida");
+                break;
+        }
+
+        Console.WriteLine("Pressione uma tecla para continuar");
+        Console.ReadLine();
     }
 
-    Console.WriteLine("Pressione uma tecla para continuar");
-    Console.ReadLine();
+    Console.WriteLine("O programa se encerrou");
+}
+catch (Exception e)
+{
+
+    Console.WriteLine($"Um erro inesperado aconteceu: {e.Message}");
 }
 
-Console.WriteLine("O programa se encerrou");
+
